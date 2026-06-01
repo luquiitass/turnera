353903d19f437bba13acbd74a2c20a870f47de0e
Lucas Larrea <lucas.larrea@mercadolibre.com>
2026-06-01 08:04:46 -0300

    Fix: add missing fields to environment.prod.ts (appName, baseDomains, etc)

--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -1,4 +1,9 @@
 export const environment = {
   production: true,
   apiUrl: 'https://turnera-api-production.up.railway.app/api',
+  appName: 'Turnera',
+  baseDomains: ['turnera.es'],
+  fallbackSlug: '',
+  barbershopId: '',
+  oneSignalAppId: '538d3c89-ea8e-4efd-8d29-88c28c878dbc',
 };
